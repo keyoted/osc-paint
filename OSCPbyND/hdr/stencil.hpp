fc66f5ee60daecc25fec0bad11d7db82f401a076
Keyoted (André Botelho) <keyoted@gmail.com>
2020-06-22 18:27:10 +0100

    Update for linux

---
+++ OSCPbyND/hdr/stencil.hpp
@@ -0,0 +1,14 @@
+//
+//  stencil.hpp
+//  OSCPbyND
+//
+//  Created by NeonDregs on 24/05/2018.
+//  Copyright © 2018 NeonDregs. All rights reserved.
+//
+
+#ifndef stencil_hpp
+#define stencil_hpp
+
+#include <stdio.h>
+
+#endif /* stencil_hpp */
